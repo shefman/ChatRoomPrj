2111009ae407c20475266dfebcb881d4a0b9b415
Shautsou_A <shefman@mail.ru>
2016-12-08 17:40:21 +0300

    lab14

--- ChatRoomPrj/src/ChatClient.java
+++ ChatRoomPrj/src/ChatClient.java
@@ -6,17 +6,28 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 public class ChatClient {
 	
 	private JTextArea output;
 	private JTextField input;
+	
 	private JButton sendButton;
 	private JButton quitButton;
+	
+	private JComboBox username;
+	
+	private JDialog aboutDialog;
+	private JFrame frame;
+	private JScrollPane textPane;
 	
 	public ChatClient(){
 		output = new JTextArea(10, 50);
@@ -24,12 +35,20 @@ public class ChatClient {
 		input = new JTextField(50);
 		sendButton = new JButton("Send");
 		quitButton = new JButton("Quit");
+		
+		username = new JComboBox();
+		username.addItem("Andrey Pupkin");
+		username.addItem("Pup88");
+		username.addItem("Andy");
+		
+		textPane = new JScrollPane(output, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
 	}
-	
+		
 	private class SendHandler implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			String text = input.getText();
-			output.append(text+"\n");
+			output.append(username.getSelectedItem()+ ": "+text+"\n");
 			input.setText("");
 		}
 	}
@@ -42,7 +61,7 @@ public class ChatClient {
 
 	
 	public void launchFrame(){
-		JFrame frame = new JFrame("Chat Room");
+		frame = new JFrame("Chat Room");
 		
 		frame.setLayout(new BorderLayout());
 		
@@ -50,11 +69,13 @@ public class ChatClient {
 		frame.add(input, BorderLayout.SOUTH);
 		
 		JPanel p1 = new JPanel();
-		p1.setLayout(new GridLayout(2, 1));
+		p1.setLayout(new GridLayout(3, 1));
 		p1.add(sendButton);
 		p1.add(quitButton);
+		p1.add(username);
 		
 		frame.add(p1,BorderLayout.CENTER);
+		output.add(textPane, BorderLayout.WEST);
 		
 		frame.pack();
 		frame.setVisible(true);	
@@ -64,7 +85,6 @@ public class ChatClient {
 		frame.addWindowListener(new CloseHandler());
 		quitButton.addActionListener(new ActionListener() {
 			
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				System.exit(0);
